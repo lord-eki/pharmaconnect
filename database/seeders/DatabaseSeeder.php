de85435ff5c5e903a42c15d509de30d23b115b00
brian lodeki <lodekib@gmail.com>
2025-10-12 08:18:05 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
      $this->call([
-        RolesAndPermissionsSeeder::class
+        RolesAndPermissionsSeeder::class,
+        UserSeeder::class         
      ]);
     }
 }
