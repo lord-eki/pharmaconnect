f8c0bd2ca65ca748ba7abdae2b91ce4229f45dc1
brian lodeki <lodekib@gmail.com>
2025-10-12 08:47:11 +0300

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,21 +14,7 @@ class UserSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-
-        // Create permissions and assign them to Admin role
-        $permissions = [
-            'view users',
-            'create users',
-            'edit users',
-            'delete users',
-        
-        ];
-
-        foreach ($permissions as $permission) {
-            $perm = Permission::firstOrCreate(['name' => $permission]);
-            $adminRole->givePermissionTo($perm);
-        }
-
+    {
         // Create Admin User
         $admin = User::firstOrCreate(
             ['email' => 'admin@pharmaconnect.com'],
