dd0e251cba3de5c075eea6a0b06258609a676d88
brian lodeki <lodekib@gmail.com>
2025-12-18 22:37:29 +0300

    FEAT::Invoices on LPOs

--- database/migrations/2025_12_18_112155_create_invoices_table.php
+++ database/migrations/2025_12_18_112155_create_invoices_table.php
@@ -11,6 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('invoices');   
+
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
             $table->string('invoice_number')->unique();
