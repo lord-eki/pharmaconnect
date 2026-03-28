f51b44641ed3b8993196dd4984c0020693c1d5da
brian lodeki <lodekib@gmail.com>
2026-03-29 00:58:43 +0300

    Fix active status on form templates

---
+++ database/migrations/2026_03_29_003835_ass_is_active_column_to_insurance_templates.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (! Schema::hasColumn('insurance_form_templates', 'is_active')) {
+            Schema::table('insurance_form_templates', function (Blueprint $table) {
+                $table->boolean('is_active')->default(false)->after('version');
+            });
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('insurance_form_templates', function (Blueprint $table) {
+            //
+        });
+    }
+};
